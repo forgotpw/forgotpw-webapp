1f3bcc6f1759e94a3e7d4ac4455d61d346703da2
Doug Kerwin <dwkerwin@gmail.com>
2018-08-29 23:17:01 -0400

    password secret service wip

--- src/app/password-hint-store-form/password-hint-store-form.component.ts
+++ src/app/password-hint-store-form/password-hint-store-form.component.ts
@@ -1,20 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { PasswordHintStoreRequest } from '../password-hint-store-request';
+import { PasswordSecretsService } from '../password-secrets.service';
 
 @Component({
   selector: 'app-password-hint-store-form',
   templateUrl: './password-hint-store-form.component.html',
-  styleUrls: ['./password-hint-store-form.component.css']
+  styleUrls: ['./password-hint-store-form.component.css'],
+  providers: [PasswordSecretsService]
 })
 export class PasswordHintStoreFormComponent implements OnInit {
   model = new PasswordHintStoreRequest('', '', '');
+  working = false;
 
-  constructor() { }
+  constructor(private passwordSecretsService: PasswordSecretsService) { }
 
   ngOnInit() {
   }
 
   onSubmit() {
+    this.working = true;
+    console.log('Starting request')
+    this.passwordSecretsService.storePasswordHint(this.model)
+    .subscribe(() => {
+      console.log('Finished request')
+      this.working = false;
+    })
   }
 
 }
