b8ede43059aefed57861cd1a255227d0a3074f00
Doug Kerwin <dwkerwin@gmail.com>
2018-09-05 23:23:24 -0400

    converted input fields to material design form controls

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule }   from '@angular/forms'
+import { FormsModule, ReactiveFormsModule }   from '@angular/forms'
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
@@ -8,6 +8,7 @@ import { PasswordHintStoreFormComponent } from './password-hint-store-form/passw
 import { NgxSpinnerModule } from 'ngx-spinner';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatRadioModule } from '@angular/material/radio';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [
@@ -17,10 +18,12 @@ import { MatRadioModule } from '@angular/material/radio';
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
     HttpClientModule,
     NgxSpinnerModule,
     BrowserAnimationsModule,
-    MatRadioModule
+    MatRadioModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/password-hint-store-form/password-hint-store-form.component.ts
+++ src/app/password-hint-store-form/password-hint-store-form.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { PasswordHintStoreRequest } from '../password-hint-store-request';
 import { PasswordSecretsService } from '../password-secrets.service';
 import { NgxSpinnerService } from 'ngx-spinner';
+import { FormControl, FormGroupDirective, NgForm, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-password-hint-store-form',
@@ -12,6 +13,22 @@ import { NgxSpinnerService } from 'ngx-spinner';
 export class PasswordHintStoreFormComponent implements OnInit {
   model = new PasswordHintStoreRequest('', '', '');
 
+  applicationFormControl = new FormControl('', [
+    Validators.required,
+    Validators.minLength(3),
+    Validators.maxLength(100)
+  ]);
+  phoneFormControl = new FormControl('', [
+    Validators.required,
+    Validators.minLength(10),
+    Validators.maxLength(20)
+  ]);
+  hintFormControl = new FormControl('', [
+    Validators.required,
+    Validators.minLength(3),
+    Validators.maxLength(100)
+  ]);
+
   constructor(
     private passwordSecretsService: PasswordSecretsService,
     private spinner: NgxSpinnerService) { }
@@ -21,6 +38,11 @@ export class PasswordHintStoreFormComponent implements OnInit {
 
   onSubmit() {
     this.spinner.show();
+
+    this.model.application = this.applicationFormControl.value;
+    this.model.phone = this.phoneFormControl.value;
+    this.model.hint = this.hintFormControl.value;
+    
     this.passwordSecretsService.storePasswordHint(this.model)
     .subscribe(() => {
       this.spinner.hide();
