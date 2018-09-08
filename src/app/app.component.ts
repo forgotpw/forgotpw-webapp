537a77c1b8e61136de3c64ca8642be792ad921d4
Doug Kerwin <dwkerwin@gmail.com>
2018-09-08 00:20:38 -0400

    switched to reactive forms, buttons on main menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,42 @@
-import { Component } from '@angular/core';
-import { MdRadioChange } from '@angular/material';
+import { Component, OnInit } from '@angular/core';
+// import { MdRadioChange } from '@angular/material';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  modes: Array<any> = [
-    {
-      value: 'STORE',
-      desc:  'Store a password hint'
-    },
-    {
-      value: 'RETRIEVE',
-      desc:  'Retrieve a password hint'
-    }
-  ];
-  selectedMode: string = this.modes[0].value;
+export class AppComponent implements OnInit {
+  // modes: Array<any> = [
+  //   {
+  //     value: 'STORE',
+  //     desc:  'Store a password hint'
+  //   },
+  //   {
+  //     value: 'RETRIEVE',
+  //     desc:  'Retrieve a password hint'
+  //   }
+  // ];
+  // selectedMode: string = this.modes[0].value;
+  // radioChange(event: MdRadioChange) {
+  //   //console.log(event.value);
+  // }
+  selectedMode: string;
 
-  radioChange(event: MdRadioChange) {
-    //console.log(event.value);
+  ngOnInit() {
+    this.selectedMode = 'MENU';
+  }
+
+  onStoreClick() {
+    this.selectedMode = 'STORE';
+  }
+
+  onRetrieveClick() {
+    this.selectedMode = 'RETRIEVE';
+  }
+
+  onStoreSubmitted() {
+    this.selectedMode = 'MENU';
   }
 
 }
--- src/app/password-hint-store-form/password-hint-store-form.component.ts
+++ src/app/password-hint-store-form/password-hint-store-form.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output, ViewChild } from '@angular/core';
 import { PasswordHintStoreRequest } from '../password-hint-store-request';
 import { PasswordSecretsService } from '../password-secrets.service';
 import { NgxSpinnerService } from 'ngx-spinner';
-import { FormControl, FormGroupDirective, NgForm, Validators } from '@angular/forms';
+import { FormControl, FormBuilder, FormGroup, FormGroupDirective, NgForm, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-password-hint-store-form',
@@ -11,45 +11,64 @@ import { FormControl, FormGroupDirective, NgForm, Validators } from '@angular/fo
   providers: [PasswordSecretsService]
 })
 export class PasswordHintStoreFormComponent implements OnInit {
-  model = new PasswordHintStoreRequest('', '', '');
-
-  applicationFormControl = new FormControl('', [
-    Validators.required,
-    Validators.minLength(3),
-    Validators.maxLength(100)
-  ]);
-  phoneFormControl = new FormControl('', [
-    Validators.required,
-    Validators.minLength(10),
-    Validators.maxLength(20)
-  ]);
-  hintFormControl = new FormControl('', [
-    Validators.required,
-    Validators.minLength(3),
-    Validators.maxLength(100)
-  ]);
+  @Output() submitted = new EventEmitter<boolean>();
+  private storeForm: FormGroup;
 
   constructor(
     private passwordSecretsService: PasswordSecretsService,
-    private spinner: NgxSpinnerService) { }
+    private spinner: NgxSpinnerService,
+    private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.storeForm = this.formBuilder.group({
+      application: [
+          '', [
+            Validators.required,
+            Validators.minLength(3),
+            Validators.maxLength(100)
+          ]
+      ],
+      phone: [
+        '', [
+          Validators.required,
+          Validators.minLength(10),
+          Validators.maxLength(20)
+        ]
+      ],
+      hint: [
+        '', [
+          Validators.required,
+          Validators.minLength(3),
+          Validators.maxLength(100)
+        ]
+      ]
+    });
   }
 
-  onSubmit() {
+  get f() { return this.storeForm.controls; }
+
+  onSubmit(storeFormDirective: FormGroupDirective) {
+    if (this.storeForm.invalid) {
+      return;
+    }
     this.spinner.show();
 
-    this.model.application = this.applicationFormControl.value;
-    this.model.phone = this.phoneFormControl.value;
-    this.model.hint = this.hintFormControl.value;
-    
-    this.passwordSecretsService.storePasswordHint(this.model)
+    let model = new PasswordHintStoreRequest('', '', '');
+    model.application = this.f.application.value;
+    model.phone = this.f.phone.value;
+    model.hint = this.f.hint.value;
+
+    this.passwordSecretsService.storePasswordHint(model)
     .subscribe(() => {
       this.spinner.hide();
-      this.model.application = '';
-      this.model.phone = '';
-      this.model.hint = '';
+      storeFormDirective.reset();
+      this.storeForm.reset();
+      this.submitted.emit(true);
     })
+  }
+
+  onCancel() {
+    this.submitted.emit(false);
   }
 
 }
