a82215f6488eca1c385b8fb3f6954e0065073d87
Doug Kerwin <dwkerwin@gmail.com>
2018-10-10 01:28:43 -0400

    promoted terraform to prod

--- terraform/prod/vars.tf
+++
@@ -1,4 +0,0 @@
-# used to hide the development website from search engines / users
-variable "website_allowed_ip_range" {
-  description = "The CIDR range allowed to view the website. Use 0.0.0.0/0 for prod."
-}
